bf2344c2b89f957ef4aca1424e53e4c706a5e139
Elias-Cz <cazessus.elias@gmail.com>
2020-06-22 15:55:06 -0700

    more ui fixes + loading animation

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+var load;
+
+function pageLoader() {
+  load = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.querySelector('.row').style.display = "flex"
+  document.querySelector('.load').style.display = "none"
+}
+
 // adds the enter key to be used by the search box
 const searchbox = document.querySelector('.searchbox');
 searchbox.addEventListener('keypress', setQuery);
@@ -15,6 +26,11 @@ async function getData() {
 
   let response = await fetch(`${api_url}total/country/${searchbox.value}`)
   let result = await response.json();
+  console.log(result)
+  if (result.message == "Not Found") {
+    alert('Country not found')
+    document.querySelector('.searchbox').value = ""
+  } else {
 
 //logs data
 console.log(result[result.length - 1].Deaths);
@@ -26,7 +42,9 @@ const months = ["JANUARY", "FEBRUARY", "MARCH","APRIL", "MAY", "JUNE", "JULY", "
 let latest = result[result.length - 1].Date
 let current_month = months[latest[5, 6] -1]
 console.log(current_month)
-let date = latest[latest.length -12] + latest[latest.length -11]
+let date = latest[latest.length -12] + latest[latest.length -11];
+
+
 
 //displays the data for the user
 document.getElementById("country").textContent = [searchbox.value];
@@ -34,6 +52,8 @@ document.getElementById("deaths").textContent = result[result.length - 1].Deaths
 document.getElementById("confirmed").textContent = result[result.length - 1].Confirmed;
 document.getElementById("recovered").textContent = result[result.length - 1].Recovered;
 document.getElementById("date").textContent = current_month + " " + date;
+document.querySelector('.searchbox').value = ""
+}
 }
 
 //getData()
